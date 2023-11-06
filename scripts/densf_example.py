37ac9d3a1c18a2e1949a3967723467968669f141
SiebeLeDe <s.j.lekanne.deprez@vu.nl>
2023-11-06 13:26:57 +0100

    Included example

--- scripts/densf_example.py
+++ scripts/densf_example.py
@@ -1,33 +1,52 @@
-from scm.plams import Settings, DensfJob, init, finish
+from scm.plams import Settings, DensfJob, init, finish, config, KFFile
 import pathlib as pl
-from orb_visualization.densf_presets import grid, orbital, output, potential
+from orb_visualization.densf_presets import grid, orbital, output
 
-# amsview result.t41 -var SCF_A_8 -save "my_pic.png" -bgcolor "#FFFFFF" -transparent -antialias -scmgeometry "2160x1440" -wireframe
 
-# Set up paths
-current_dir = pl.Path(__file__).parent
-plams_name = "test_nh3_bh3"
-calculation = ("complex", "1")  # (name, index), e.g., (frag1, 3), (frag2, 1), (complex, 2)
-output_dirname = "output_files"
+# ------------------Available test files------------------
+class Restricted_TestFiles:
+    FILE1 = "restricted_largecore_differentfragsym_c4v_full"
+    FILE2 = "restricted_largecore_differentfragsym_c4v_full"
+    FILE3 = "restricted_largecore_fragsym_c3v_nonrelativistic_full"
+    FILE4 = "restricted_largecore_fragsym_c3v_nonrelativistic_full"
+    FILE5 = "restricted_nocore_fragsym_c3v_full"
+    FILE6 = "restricted_nocore_fragsym_nosym_full"
+
+
+# --------------------Input Arguments-------------------- #
+current_path = pl.Path(__file__).parent
+path_to_folder_with_rkf_files = (current_path.parent / "test" / "fixtures" / "rkfs")
+rkf_file = "restricted_nocore_fragsym_nosym_full"
+output_dirname = "densf_output"
+plams_foldername = "densf_calc"
 
 # Set up directories
-output_dir = current_dir / output_dirname
-pyfrag_dir = current_dir / "NH3_BH3" / "nh3_bh3"
-rkf_path = pyfrag_dir / f"{calculation[0]}.{calculation[1].zfill(5)}" / "adf.rkf"
+output_dir = current_path / output_dirname
+rkf_path = path_to_folder_with_rkf_files / f"{rkf_file}.adf.rkf"
 
 # DensF settings
 set = Settings()
 set.update(grid(grid_type="fine"))
 set.update(output(outputfile=str(output_dir / "result")))
 set.update(orbital(all_option=(1, 2)))
-set.update(potential("xc", True))
 
 # Make sure the output dir exists
 output_dir.mkdir(exist_ok=True)
 
 # Run DensF
-init(path=current_dir, folder=plams_name)
+init(path=current_path, folder=plams_foldername)
 job = DensfJob(inputjob=rkf_path, settings=set)
 job.run()
-print(job.get_input())
+# print(job.get_input())
+if job.ok():
+    config.erase_workdir = True
 finish()
+
+# plot orbital
+outfile = output_dir / "result.t41"
+print(KFFile(outfile).sections())
+# print(KFFile(outfile).read_section("SCF_A"))
+orb_specifier = "SCF_A_11"
+# plot_settings = AMSViewPlotSettings(scmgeometry="1920x1080", zoom=1.5)
+
+# plot_orbital_with_amsview(outfile, orb_specifier, save_file=output_dir / f"{orb_specifier}.png")
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
-from pprint import pprint
 import pathlib as pl
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -8,18 +8,28 @@ from orb_analysis.complex import create_calc_analyser
 np.set_printoptions(precision=5, suppress=True)
 # https://www.scm.com/doc/ADF/Appendices/TAPE21.html
 
+
+# ------------------Available test files------------------ #
+class Restricted_TestFiles:
+    FILE1 = "restricted_largecore_differentfragsym_c4v_full"
+    FILE2 = "restricted_largecore_differentfragsym_c4v_full"
+    FILE3 = "restricted_largecore_fragsym_c3v_nonrelativistic_full"
+    FILE4 = "restricted_largecore_fragsym_c3v_nonrelativistic_full"
+    FILE5 = "restricted_nocore_fragsym_c3v_full"
+    FILE6 = "restricted_nocore_fragsym_nosym_full"
+
+
 # --------------------Input Arguments-------------------- #
-current_path = pl.Path(__file__)
-path_to_folder_with_rkf_files = (
-    current_path.parent.parent / "test" / "fixtures" / "rkfs"
-)
-rkf_file = "restricted_nocore_fragsym_nosym_full"
+current_path = pl.Path(__file__).parent
+path_to_folder_with_rkf_files = (current_path.parent / "test" / "fixtures" / "rkfs")
+# See the test/fixtures/rkfs folder for more examples
+rkf_file = Restricted_TestFiles.FILE5
 path_to_rkf_file = path_to_folder_with_rkf_files / f"{rkf_file}.adf.rkf"
 
 # --------------------Main-------------------- #
 calc_analyzer = create_calc_analyser(path_to_rkf_file)
 
-# # NoSym calculation
+# # NoSym calculation with fragment symmetry
 # frag1_labels = [
 #     "4_AA",
 #     "5_AA",
@@ -34,7 +44,7 @@ calc_analyzer = create_calc_analyser(path_to_rkf_file)
 #     "2_AAA",
 # ]
 
-# C3v calculation
+# # Cs calculation with fragment symmetry
 # frag1_labels = [
 #     "33_AA",
 #     "29_AA",
@@ -45,32 +55,49 @@ calc_analyzer = create_calc_analyser(path_to_rkf_file)
 #     "30_AA",
 # ]
 
-# NoSym calculation
+# c3v calculation with fragment symmetry
 frag1_labels = [
-    "1_A",
+    "7_A1",  # HOMO-1
+    "8_A1",  # HOMO
+    "9_A1",  # LUMO
+]
+frag2_labels = [
+    "13_A1",  # HOMO
+    "14_A1",  # LUMO
+    "15_A1",  # LUMO-1
 ]
 
-frag2_labels = [
-    "1_A",
-]
+# NoSym calculation without fragment symmetry
+# frag1_labels = [
+#     "1_A",
+# ]
+
+# frag2_labels = [
+#     "1_A",
+# ]
 
 print("MAIN FUNCTION")
-[pprint(frag.fragment_data.n_frozen_cores_per_irrep) for frag in calc_analyzer.fragments]
-[pprint(frag.fragment_data.gross_populations) for frag in calc_analyzer.fragments]
-[pprint(frag.fragment_data.orb_energies) for frag in calc_analyzer.fragments]
+# [pprint(frag.fragment_data.n_frozen_cores_per_irrep) for frag in calc_analyzer.fragments]
+# [pprint(frag.fragment_data.gross_populations) for frag in calc_analyzer.fragments]
+# [pprint(frag.fragment_data.orb_energies) for frag in calc_analyzer.fragments]
 
-# # Print the label, energy, and population of each SFO in each fragment
-# frag_counter = 1
-# for frag, frag_label in zip(calc_analyzer.fragments, [frag1_labels, frag2_labels]):
-#     name = frag.name
-#     for sfo in frag_label:
-#         orb_energy = calc_analyzer.get_orbital_energy(fragment=frag_counter, sfo=sfo)
-#         gross_pop = calc_analyzer.get_gross_population(fragment=frag_counter, sfo=sfo)
-#         print(f"Fragment {frag_counter}, SFO {sfo :6s}: {orb_energy :^+.4f} Ha, {gross_pop :<.5f} electrons")
-#     frag_counter += 1
+# Print the label, energy, and population of each SFO in each fragment
+frag_counter = 1
+for frag, frag_label in zip(calc_analyzer.fragments, [frag1_labels, frag2_labels]):
+    name = frag.name
+    for sfo in frag_label:
+        orb_energy = calc_analyzer.get_orbital_energy(fragment=frag_counter, sfo=sfo)
+        gross_pop = calc_analyzer.get_gross_population(fragment=frag_counter, sfo=sfo)
+        print(f"Fragment {frag_counter}, SFO {sfo :6s}: {orb_energy :^+.4f} Ha, {gross_pop :<.5f} electrons")
+    frag_counter += 1
 
 overlap = np.array([
     [calc_analyzer.get_overlap(sfo1=label1, sfo2=label2) for label2 in frag2_labels]
     for label1 in frag1_labels
 ])
-# print(overlap.flatten())
+
+plt.imshow(overlap, cmap="rainbow", interpolation="nearest", alpha=0.5)
+plt.colorbar()
+plt.xticks(np.arange(len(frag1_labels)), frag1_labels)
+plt.yticks(np.arange(len(frag2_labels)), frag2_labels)
+plt.show()
