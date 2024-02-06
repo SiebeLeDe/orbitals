0ccb4fd7750dc6bc9e0eaaa8721009dbdb33e07a
SiebeLeDe <siebbe@hotmail.com>
2024-02-06 18:13:17 +0100

    added orbital pairs

--- scripts/debug_script.py
+++ scripts/debug_script.py
@@ -10,10 +10,10 @@ np.set_printoptions(precision=5, suppress=True)
 
 # --------------------Input Arguments-------------------- #
 rkf_folder = pl.Path(__file__).parent.parent / "test" / "fixtures" / "rkfs"
-rkf_file = rkf_folder / "unrestricted_largecore_fragsym_nosym_full.adf.rkf"
+rkf_file = rkf_folder / "restricted_largecore_fragsym_c3v_full.adf.rkf"
 # rkf_file = rkf_folder / "unrestricted_largecore_fragsym_c3v_full.adf.rkf"
 
-orb_range: tuple[int, int] = (6, 6)  # The range of orbitals to be analyzed (HOMO-X, LUMO+X with X the specified range)
+orb_range: tuple[int, int] = (3, 3)  # The range of orbitals to be analyzed (HOMO-X, LUMO+X with X the specified range)
 irrep: str | None = None  # irrep such as "A1" (C3v) or "AA" (in Cs)
 spin: str = SpinTypes.A  # "A" or "B" | does only matter for unrestricted calculations
 
@@ -22,19 +22,7 @@ spin: str = SpinTypes.A  # "A" or "B" | does only matter for unrestricted calcul
 # Creating the calc_analyzer object that performs the analysis of the (fragment) calculation
 calc_analyzer = create_calc_analyser(rkf_file)  # This is the main object that will be used to analyze the calculation
 
-# Printing the results
-fragment1, fragment2 = calc_analyzer.fragments
-
-# Print A and B values next to each other for each irrep
-pop_A, pop_B = fragment1.fragment_data.gross_populations.values()
-print(pop_A)
-
-frag1_pop_2A = fragment1.get_gross_population(irrep="A1", index=4, spin="A")
-frag1_pop_2B = fragment1.get_gross_population(irrep="A1", index=4, spin="B")
-# print(frag1_pop_2A)
-# print(frag1_pop_2B)
-
-# for (irrep, pops), (irrepB, popsB) in zip(pop_A.items(), pop_B.items()):
-#     print(irrep)
-#     for pop, popB in zip(pops, popsB):
-#         print(f"{pop:6.3f}, {popB:6.3f}")
+sfo_manager = calc_analyzer.get_sfo_orbitals(orb_range, orb_range)
+print(sfo_manager)
+orb_pairs = sfo_manager.get_most_destabilizing_pauli_pairs(9, "HOMO_LUMO")  # type: ignore
+[print(pair) for pair in orb_pairs]
